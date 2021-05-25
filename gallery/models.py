9def5eb0bfc00a8cee368d836e9b5dbbbc1e7113
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-25 15:31:17 -0300

    configurated pillow, added models, migrations made

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Photos(models.Model):
+    name = models.CharField(max_length=50)
+    about = models.CharField(max_length=255, blank=True)
+    date = models.DateTimeField(timezone.now)
+    source = models.ImageField(upload_to='photos/%Y/%m/')
