db8d3dff02860b5475d6a744d6bbd3cbb4422cfe
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-26 01:15:53 -0300

    updated model and admin

--- gallery/models.py
+++ gallery/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 
-class Photos(models.Model):
+class Pics(models.Model):
     name = models.CharField(max_length=50)
     about = models.CharField(max_length=255, blank=True)
-    date = models.DateTimeField(timezone.now)
+    date = models.DateTimeField(default=timezone.now)
     source = models.ImageField(upload_to='photos/%Y/%m/')
+
+    def __str__(self):
+        return self.name
