ad0e68f663e9b428b5c6072566b6433a87243a0c
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-26 02:31:09 -0300

    updated admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Pics
 
-admin.site.register(Pics)
+
+class PicsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'about', 'date')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+admin.site.register(Pics, PicsAdmin)
